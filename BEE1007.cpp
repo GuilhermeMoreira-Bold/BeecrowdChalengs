21b1b82f48e32e265ea83c2bd4b14e3a698a5aaa
Guilherme Moreira <51029818+GuilhermeMoreira-Bold@users.noreply.github.com>
2024-02-24 17:32:25 -0300

    BEE1007

---
+++ BEE1007.cpp
@@ -0,0 +1,18 @@
+//
+// Created by gulhe on 24/02/2024.
+//
+#include <iostream>
+
+int main(){
+
+    int A,B,C,D;
+
+    std::cin >> A;
+    std::cin >> B;
+    std::cin >> C;
+    std::cin >> D;
+
+    std::cout << "DIFERENCA = " << A*B - C*D << std::endl;
+
+    return 0;
+}
